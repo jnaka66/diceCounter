eb2485eb2d87c69416de90fab645553a93daead1
jnaka66 <60413339+jnaka66@users.noreply.github.com>
2022-11-29 11:16:41 -0500

    added recent rolls, consider this V1

--- diceCounter.py
+++ diceCounter.py
@@ -1,13 +1,15 @@
 import PySimpleGUI as sg
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import numpy as np
 
 
-numbers = [1,2,3,4,5,6,7,8,9,10,11,12]
-rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
+numbers = range(1,13)
+rolls = list(np.zeros(12).astype('int'))
+historyRolls=[]
 
 def createBarGraph(numbers, rolls):
-    fig = plt.figure(figsize =(32, 18))
+    fig = plt.figure(figsize =(25, 15))
     rects = plt.bar(numbers, rolls, color='red', width=0.4)
     plt.xticks(numbers, fontsize=20, weight = 'bold')
     plt.title('Dice Rolls', fontsize=14)
@@ -15,6 +17,7 @@ def createBarGraph(numbers, rolls):
     plt.ylabel('Count', fontsize=14)
     plt.yticks([0,1],fontsize=20, weight = 'bold')
     plt.grid(visible=True, which='major', axis='y')
+    fig.tight_layout()
     return fig, rects
 
 
@@ -25,6 +28,7 @@ def updateBarGraph(rolls, figure, rects):
         i+=1
     plt.ylim([0, max(rolls)])
     plt.yticks(range(max(rolls)+1))
+    fig.tight_layout()
     fig.canvas.draw()
 
 
@@ -38,7 +42,8 @@ def draw_figure(canvas, figure):
 
 sg.theme('LightBlue')   # Add a touch of color
 # All the stuff inside your window.
-layout = [  [sg.Text('Catan Dice Stats')],
+layout = [  [sg.Text('Catan Dice Stats', font=("Helvetica", 25))],
+            [sg.Text('Last 10 Rolls', key='-HISTORY-', font=("Helvetica", 50))],
             [sg.Canvas(key='-CANVAS-')],
             [sg.Input('', enable_events=True,  key='-INPUT-', )],
             [sg.Button('Submit', visible=False, bind_return_key=True)],
@@ -46,7 +51,7 @@ layout = [  [sg.Text('Catan Dice Stats')],
 
 # Create the Window
 window = sg.Window('Catan Dice Counter', layout,finalize=True, element_justification='center', resizable=True)
-window.maximize()
+#window.maximize()
 fig, rects = createBarGraph(numbers,rolls)
 draw_figure(window['-CANVAS-'].TKCanvas, fig)
 
@@ -60,11 +65,13 @@ while True:
         window['-INPUT-'].update('')
     elif event == 'Submit':
         #print(int(window['-INPUT-'].get()) in numbers)
-        if(int(window['-INPUT-'].get()) in numbers):
+        if(window['-INPUT-'].get() != '' and int(window['-INPUT-'].get()) in numbers):
             print('You have submited %s'% window['-INPUT-'].get())
             rolls[int(window['-INPUT-'].get())-1]+=1
+            historyRolls.append(int(window['-INPUT-'].get()))
             updateBarGraph(rolls, fig, rects)
             window['-INPUT-'].update('')
+            window['-HISTORY-'].update('Last Ten Rolls' + str(historyRolls[-10:]))
         else:
             window['-INPUT-'].update('')
 
